a98d8d3492f21ab578a7d2dda11997c0d82cf9d9
Mateo Morera <moreramateo@gmail.com>
2020-06-20 05:29:07 -0500

    JM - añadi la logica de variables

--- App.js
+++ App.js
@@ -1,92 +1,92 @@
-function Empleado(cedul, salario, dia) {
-  
+class Empleado {
+    constructor(cedul, salario, dia) {
+        this.cedul = cedul;
+        this.salario = salario;
+        this.dia = dia;
 
-    this.cedul = cedul;
-    this.salario = salario;
-    this.dia = dia;
+        this.salariodia = salario * dia;
+        this.auxTrans = 0;
+
+        if (salario < 1400000) {
+            var aux = (100000 / 30) * dia;
+            this.auxTrans = aux;
+        } else {
+            this.auxTrans = 0;
+        }
+
+    };
 
 
-    this.salariodia = this.dia * this.salario;
-   this.salariodia1 = function(){
-      this.dia * this.salario;
-     }
-      
-    }
-
-
- 
-
-
+};
 
 
 class UI {
 
+    addEmpleado(empleado) {
+        const empleadoList = document.getElementById('empleado-list');
+        const element = document.createElement('div');
 
-  addEmpleado(empleado) {
-    const empleadoList = document.getElementById('empleado-list');
-    const element = document.createElement('div');
+        //return salariodia;
 
-    //return salariodia;
-
-    element.innerHTML = `
+        element.innerHTML = `
           <div class = "card text-center mb-4">
               <div class = "card-body">
                <strong>Cedula</strong>: ${empleado.cedul}
                <strong>Salario</strong>: ${empleado.salario}
                <strong>dias</strong>: ${empleado.dia} 
                <strong>Salario</strong>: ${empleado.salariodia} 
-               <strong>Salario</strong>: ${empleado.salariodia1}    
+               <strong>Auxilio de Transporte</strong>: ${empleado.auxTrans}   
                <a href = "#" class = "btn btn-danger" name="delete" >Delete</a>           
           </div>
             </div>
         `;
-    empleadoList.appendChild(element);
-    this.resetForm();
-
-  }
-
-
-
-  resetForm() {
-
-    document.getElementById('empleado-form').reset();
-
-
-  }
-
-
-  deleteEmpleado(element) {
-
-    if (element.name === 'delete') {
-      element.parentElement.parentElement.parentElement.remove();
-      this.showMessage('Empleado Deleted Successfully', 'danger');
+        empleadoList.appendChild(element);
+        this.resetForm();
 
     }
 
 
 
-  }
+    resetForm() {
 
-  showMessage(message, cssClass) {
-    const div = document.createElement('div');
-    div.className = `alert alert-${cssClass} mt-2`;
-    div.appendChild(document.createTextNode(message));
-    //Showing in DOM
-    const container = document.querySelector('.container');
-    const app = document.querySelector('#App');
-    container.insertBefore(div, app);
-    setTimeout(function () {
-      document.querySelector('.alert').remove();
+        document.getElementById('empleado-form').reset();
 
-    }, 2000)
-  }
+
+    }
+
+
+    deleteEmpleado(element) {
+
+        if (element.name === 'delete') {
+            element.parentElement.parentElement.parentElement.remove();
+            this.showMessage('Empleado Deleted Successfully', 'danger');
+
+        }
+
+
+
+    }
+
+    showMessage(message, cssClass) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${cssClass} mt-2`;
+        div.appendChild(document.createTextNode(message));
+        //Showing in DOM
+        const container = document.querySelector('.container');
+        const app = document.querySelector('#App');
+        container.insertBefore(div, app);
+        setTimeout(function() {
+            document.querySelector('.alert').remove();
+
+        }, 2000)
+    }
 
 
 }
 
 //DOM Events
 document.getElementById('empleado-form').
-  addEventListener('submit', function (e) {
+addEventListener('submit', function(e) {
     const cedul = document.getElementById('cedul').value;
     const salario = document.getElementById('salario').value;
     const dia = document.getElementById('dia').value;
@@ -98,7 +98,7 @@ document.getElementById('empleado-form').
     const ui = new UI();
 
     if (cedul === '' || salario === '' || dia === '') {
-      return ui.showMessage('Complete Fields Please', 'danger');
+        return ui.showMessage('Complete Fields Please', 'danger');
 
     }
     ui.addEmpleado(empleado);
@@ -108,12 +108,12 @@ document.getElementById('empleado-form').
     e.preventDefault();
 
 
-  });
+});
 
-document.getElementById('empleado-list').addEventListener('click', function (e) {
+document.getElementById('empleado-list').addEventListener('click', function(e) {
 
-  const ui = new UI();
-  ui.deleteEmpleado(e.target);
+    const ui = new UI();
+    ui.deleteEmpleado(e.target);
 
 
 
